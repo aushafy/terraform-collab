f708110731f3c2a7e345367ad5477a87354f3425
Yusuf Luai <yusuf.luai01@gmail.com>
2021-12-28 21:52:38 +0700

    commit init

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,7 @@ module "ec2-instance" {
   associate_public_ip_address   = var.ec2_public_ip
   availability_zone             = "ap-southeast-2c"
   ami                    = "ami-05654370f5b5eb0b0"
-  instance_type          = "t3.micro"
+  instance_type          = "t2.micro"
   key_name               = "devops-aushafy"
   monitoring             = true
   vpc_security_group_ids = ["sg-c983c6bb","sg-0755013b26a648f57"]
